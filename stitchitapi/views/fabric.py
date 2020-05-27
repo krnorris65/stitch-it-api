bbefc70d05f18e3cfffa74584837cf32a3f4b399
Kristen Norris <krnorris65@gmail.com>
2020-05-27 11:31:48 -0500

    created viewset for fabric for retrive and list

---
+++ stitchitapi/views/fabric.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from stitchitapi.models import Fabric
+
+class FabricSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for fabric
+
+    Arguments:
+        serializers.HyperlinkedModelSerializer
+    """
+    class Meta:
+        model = Fabric
+        url = serializers.HyperlinkedIdentityField(
+            view_name='fabric',
+            lookup_field='id'
+        )
+        fields = ('id', 'type', 'count')
+
+class Fabrics(ViewSet):
+    """Fabrics for Stitch It"""
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single fabric
+
+        Returns:
+            Response -- JSON serialized fabric instance
+        """
+        try:
+            fabric = Fabric.objects.get(pk=pk)
+            serializer = FabricSerializer(fabric, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+    
+    def list(self, request):
+        """Handle GET requests to fabrics resource
+
+        Returns:
+            Response -- JSON serialized list of fabrics
+        """
+        fabrics = Fabric.objects.all()
+        serializer = FabricSerializer(
+            fabrics,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
