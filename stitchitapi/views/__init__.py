c4eadb4eb7071202e6c722c6006d20247bf9eb7e
Kristen Norris <krnorris65@gmail.com>
2020-06-08 11:51:39 -0500

    set up initial viewset with serializer, retrieve and list

--- stitchitapi/views/__init__.py
+++ stitchitapi/views/__init__.py
@@ -1,4 +1,5 @@
 from .fabric import Fabrics
 from .size import Sizes
+from .design import Designs
 from .register import register_user
 from .register import login_user
---
+++ stitchitapi/views/design.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from stitchitapi.models import Fabric, Size, Design
+
+class DesignSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for design
+
+    Arguments:
+        serializers.HyperlinkedModelSerializer
+    """
+    class Meta:
+        model = Design
+        url = serializers.HyperlinkedIdentityField(
+            view_name='design',
+            lookup_field='id'
+        )
+        fields = ('id', 'title', 'description', 'completed_date', 'photo', 'fabric', 'size')
+        depth = 2
+
+class Designs(ViewSet):
+    """Designs for Stitch It"""
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single design
+
+        Returns:
+            Response -- JSON serialized design instance
+        """
+        try:
+            design = Design.objects.get(pk=pk)
+            serializer = DesignSerializer(design, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+    
+    def list(self, request):
+        """Handle GET requests to designs resource
+
+        Returns:
+            Response -- JSON serialized list of designs
+        """
+        designs = Design.objects.all()
+        serializer = DesignSerializer(
+            designs,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
