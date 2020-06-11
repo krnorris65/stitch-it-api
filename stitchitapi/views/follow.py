229c888cdc8d21084d50a6d2112b7132790dad0c
Kristen Norris <krnorris65@gmail.com>
2020-06-11 11:20:13 -0500

    user can only delete follows that they are the follower or stitcher on

--- stitchitapi/views/follow.py
+++ stitchitapi/views/follow.py
@@ -86,3 +86,25 @@ class Follows(ViewSet):
             return Response({}, status=status.HTTP_204_NO_CONTENT)
         else:
             return Response({'message': "Not authorized to edit this follow"}, status=status.HTTP_403_FORBIDDEN)
+    
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single follow. The user can only delete their own follows or follows they are the stitcher on
+
+        Returns:
+            Response -- 200, 403, 404, or 500 status code
+        """
+        try:
+            requesting_user = Stitcher.objects.get(user=request.auth.user)
+            follow = Follow.objects.get(pk=pk)
+            if follow.follower == requesting_user or follow.stitcher == requesting_user:
+                follow.delete()
+
+                return Response({}, status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({'message': "Not authorized to delete this follow"}, status=status.HTTP_403_FORBIDDEN)
+
+        except Follow.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
