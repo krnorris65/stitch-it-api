32cb8323fea4f9a3d36440858a7837e1c1cb5d82
Kristen Norris <krnorris65@gmail.com>
2020-06-15 12:36:48 -0500

    updated urls so user is able to see image

--- stitchit/urls.py
+++ stitchit/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework.authtoken.views import obtain_auth_token
 from stitchitapi.models import *
 from stitchitapi.views import Fabrics, Sizes, Designs, Follows, Stitchers, Users
@@ -35,3 +37,5 @@ urlpatterns = [
     path('api-token-auth/', obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
